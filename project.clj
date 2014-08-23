d0a88a6f3c8a6d1c3aa205837544342aafa5949a
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2014-08-23 14:17:06 -0400

    Extracting books from goodreads xml.

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.zip "0.1.1"]
                  [clj-http "1.0.0"]])
--- src/book_calendar/core.clj
+++ src/book_calendar/core.clj
@@ -1,8 +1,11 @@
 (ns book-calendar.core
   (:require [clj-http.client :as http]
+            [clojure.zip :as zip]
+            [clojure.data.zip :as zf]
+            [clojure.data.zip.xml :as xml-zip]
             [clojure.xml :as xml]
             [clojure.string :as string]
-            ))
+  ))
 
 (def api-key
   (System/getenv "GOODREADS_API_KEY"))
@@ -21,18 +24,34 @@
 
 (defn get-response
   [url params]
-  (xml/parse (java.io.ByteArrayInputStream.
-               (.getBytes (:body (fetch url params))))))
+  (zip/xml-zip
+    (xml/parse
+      (java.io.ByteArrayInputStream.
+        (.getBytes (:body (fetch url params)))))))
+
+(def book-attrs
+  [:title :description])
+
+(defn extract-book
+  [book-element]
+  (let [extract-attribute (fn [book-hash attr]
+                            (let [value (xml-zip/xml-> book-element zf/descendants attr xml-zip/text)]
+                              (assoc book-hash attr (string/join " " (seq value)))))]
+    (reduce extract-attribute {} book-attrs)))
 
 (defn extract-books
   [parsed-xml]
+  (let [book-list (xml-zip/xml-> parsed-xml zf/descendants :book)]
+    (map extract-book book-list))
 )
+(books-on-shelf 2003928 "currently-reading")
 
 (defn books-on-shelf
   [user-id shelf-name]
-  (get-response "/review/list" {:v 2
-                                :id user-id
-                                :shelf shelf-name
-                                :key api-key }))
+  (extract-books
+    (get-response "/review/list" {:v 2
+                                  :id user-id
+                                  :shelf shelf-name
+                                  :key api-key })))
 
-(books-on-shelf 2003928 "currently-reading")
+;(books-on-shelf 2003928 "currently-reading")
