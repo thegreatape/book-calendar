aa9b9895263375237f6373b4145c884983f9a4e6
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2014-08-23 20:25:26 -0400

    Some initial tests for book parsing.

--- src/book_calendar/core.clj
+++ src/book_calendar/core.clj
@@ -18,33 +18,42 @@
   [url params]
   (http/get (api-url url)
             {:query-params params}))
-(defn parse
-  [xml-str]
-  (xml/parse xml-str))
 
-(defn get-response
-  [url params]
+(defn xml-parse
+  [xml-string]
   (zip/xml-zip
     (xml/parse
       (java.io.ByteArrayInputStream.
-        (.getBytes (:body (fetch url params)))))))
+        (.getBytes xml-string)))))
+
+(defn get-response
+  [url params]
+  (xml-parse (:body (fetch url params))))
+
+(defn to-string [value]
+  (string/trim value))
 
 (def book-attrs
-  [:title :description])
+  {:title             to-string
+   :description       to-string
+   :image_url         to-string
+   ;:publication_year  to-string
+   ;:publication_month to-string
+   ;:publication_day   to-string
+   ;:authors           to-string
+   })
 
 (defn extract-book
   [book-element]
-  (let [extract-attribute (fn [book-hash attr]
-                            (let [value (xml-zip/xml-> book-element zf/descendants attr xml-zip/text)]
-                              (assoc book-hash attr (string/join " " (seq value)))))]
+  (let [extract-attribute (fn [book-hash [attr converter]]
+                            (let [value (xml-zip/xml1-> book-element zf/descendants attr xml-zip/text)]
+                              (assoc book-hash attr (converter value))))]
     (reduce extract-attribute {} book-attrs)))
 
 (defn extract-books
   [parsed-xml]
   (let [book-list (xml-zip/xml-> parsed-xml zf/descendants :book)]
-    (map extract-book book-list))
-)
-(books-on-shelf 2003928 "currently-reading")
+    (map extract-book book-list)))
 
 (defn books-on-shelf
   [user-id shelf-name]
@@ -53,5 +62,3 @@
                                   :id user-id
                                   :shelf shelf-name
                                   :key api-key })))
-
-;(books-on-shelf 2003928 "currently-reading")
--- test/book_calendar/core_test.clj
+++ test/book_calendar/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [book-calendar.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest parse-single-book
+  (testing "parsing a single book from goodreads review xml"
+    (let [xml (xml-parse (slurp "test/resources/reviews.xml"))
+          expected-book {:title       "Clojure for the Brave and True"
+                         :description "Clojure for the Brave and True is a work-in-progress available to read for free online."
+                         :image_url   "https://d.gr-assets.com/books/1392849558m/20873338.jpg" 
+                         }
+          actual-book (extract-book xml)]
+    (is (= expected-book actual-book)))))
