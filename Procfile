6a2cfc2e31ce18a741cdeeeb4fb32ace308383e8
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2014-08-31 20:12:09 -0400

    Heroku deployment changes: Add Procfile Add :gen-class directive Fix min-lein-version Bind to $PORT

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -jar target/book-calendar-standalone.jar
--- src/book_calendar/web.clj
+++ src/book_calendar/web.clj
@@ -2,11 +2,15 @@
   (:require [compojure.core :refer [defroutes GET]]
             [ring.adapter.jetty :as ring])
   (:use [book-calendar.core]
-        [hiccup.core]))
+        [hiccup.core])
+  (:gen-class))
 
 (defroutes routes
   (GET "/" [] 
        (html [:h1 "Hello Hiccup World"])))
 
+(def port
+  (bigdec (or (System/getenv "PORT") 8080)))
+
 (defn -main []
-  (ring/run-jetty #'routes {:port 8080 :join? false}))
+  (ring/run-jetty #'routes {:port port :join? false}))
